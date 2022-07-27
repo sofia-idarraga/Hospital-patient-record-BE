8e8206748bcc1589e9b9c39a904fd6cad7d612d0
Sofia Idarraga <sofia.idarraga@udea.edu.co>
2022-07-27 18:59:48 -0500

    trying to implement try-catch

--- src/main/java/com/sofka/hospital/controller/MedicalSpecialityController.java
+++ src/main/java/com/sofka/hospital/controller/MedicalSpecialityController.java
@@ -2,13 +2,18 @@ package com.sofka.hospital.controller;
 
 import com.sofka.hospital.dto.MedicalSpecialityDTO;
 import com.sofka.hospital.services.MedicalSpecialityService;
+import com.sofka.hospital.utility.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 
 @RestController
@@ -18,13 +23,28 @@ public class MedicalSpecialityController {
     @Autowired
     private MedicalSpecialityService medicalSpecialityService;
 
+    private Response response = new Response();
+    private HttpStatus httpStatus = HttpStatus.OK;
+
     @GetMapping("speciality")
     public List<MedicalSpecialityDTO> getAllSpecialities(){
         return medicalSpecialityService.getAllSpeciality();
     }
 
     @PostMapping("create/speciality")
-    public MedicalSpecialityDTO createSpeciality(@RequestBody MedicalSpecialityDTO medicalSpecialityDTO){
-        return medicalSpecialityService.createSpeciality(medicalSpecialityDTO);
+    public ResponseEntity<Response> createSpeciality(@RequestBody MedicalSpecialityDTO medicalSpecialityDTO){
+        response.restart();
+        try {
+            response.data = medicalSpecialityService.createSpeciality(medicalSpecialityDTO);
+            httpStatus = HttpStatus.CREATED;
+        } catch (ConstraintViolationException exception){
+            response.data = exception.getCause();
+            response.message = exception.getMessage();
+            response.error = true;
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return new ResponseEntity(response, httpStatus);
     }
+
 }
